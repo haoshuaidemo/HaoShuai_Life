ec7bd4b67b70302e4d568ada29dc0a5cacf3c05c
haoshuai <haoshuai_nuc@163.com>
2020-06-22 23:11:27 +0800

    后端提交

---
+++ blog_service/src/test/java/com/blog/blog_service/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
